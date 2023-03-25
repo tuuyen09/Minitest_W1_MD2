7aab6726c11b26e552d1830d0be1da66e478b86e
tuuyen09 <tu190901@gmail.com>
2023-03-25 19:43:09 +0700

    Minitest2

--- src/Minitest2.java
+++ src/Minitest2.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Minitest2 {
+public class Minitest2 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println("Tong cac phan tu trong mang = " + totalValue(array));
+        System.out.println("Phan tu lon nhat trong mang la: " + maxValue(array));
+    }
+
+    public static int totalValue(int[] arr) {
+        int total = 0;
+        for (int i : arr) {
+            total += i;
+        }
+        return total;
+    }
+
+    public static int maxValue(int[] arr) {
+        int max = arr[0];
+        for (int i : arr) {
+            if (max < i) {
+                max = i;
+            }
+        }
+        return max;
+    }
 }
