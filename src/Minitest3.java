44f85d73dcd79c9cd7bf2b0a0a135f76d4313c72
tuuyen09 <tu190901@gmail.com>
2023-03-25 19:43:38 +0700

    Minitest3

--- src/Minitest3.java
+++ src/Minitest3.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Minitest3 {
+import java.util.Scanner;
+
+public class Minitest3 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhap do dai cua mang:");
+        int size = input.nextInt();
+        int[] array = new int[size];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Nhap phan tu thu " + i + " cua mang:");
+            array[i] = input.nextInt();
+        }
+        for (int i : array){
+            System.out.println(i);
+        }
+    }
 }
